34b486d15e9797574c7fdf0f4e0ca170a7ab2800
Akshay Singh <akshay.singh621@gmail.com>
2016-06-13 19:49:32 -0700

    Update mergeSort.js
    Comments

--- mergeSort.js
+++ mergeSort.js
@@ -2,18 +2,22 @@ var mergeSort = function(array) {
   var length = array.length;
   var leftArray = [];
   var rightArray = [];
-  var mid = Math.floor(length / 2);
-  if (length < 2) {
+  var mid = Math.floor(length / 2); //Calculate mid
+  if (length < 2) { //If array smaller than two then can't be split so return. Recursion end.
     return;
   }
+  // Populate left and right arrays
   for (var i = 0; i < mid; i++) {
     leftArray[i] = array[i];
   }
   for (var j = mid; j < length; j++) {
     rightArray[j - mid] = array[j];
   }
+  //Call mergeSort on both arrays
   mergeSort(leftArray);
   mergeSort(rightArray);
+  
+  //Merge the two
   merge(leftArray, rightArray, array);
 }
 
@@ -31,6 +35,8 @@ var merge = function(left, right, array) {
     }
     k++;
   }
+  
+  //Whichever list is empty, flush it out to the end of the merged array
   while (i < lenLeft) {
     array[i] = left[i];
     i++;
